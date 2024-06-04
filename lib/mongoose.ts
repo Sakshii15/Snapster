3734e23dfd656281547e981d36ecd00738f5daef
Sakshii15 <sahu.sakshii015@gmail.com>
2024-06-04 16:58:16 +0530

    commit

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -37,7 +37,7 @@ export const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       
-      useUnifiedTopology: true,
+      // useUnifiedTopology: true,
     });
 
     isConnected = true;
